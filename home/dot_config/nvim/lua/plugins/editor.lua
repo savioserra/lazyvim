57ee45fe32141c25c2655e8a92b72bd908d8a6f4
Savio Serra <serra.santos.s@gmail.com>
2026-08-10 15:17:33 -0300

    map vimmove

--- home/dot_config/nvim/lua/plugins/editor.lua
+++ home/dot_config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,27 @@
 return {
-  -- Move lines and visual selections with Alt-h/j/k/l.
-  { "matze/vim-move" },
+  -- Move lines and visual selections with Alt-j/k.
+  {
+    "matze/vim-move",
+    init = function()
+      vim.g.move_map_keys = 0
+    end,
+    config = function()
+      vim.keymap.set("n", "<A-k>", "<Plug>MoveLineUp", { remap = true, silent = true, desc = "Move line up" })
+      vim.keymap.set("n", "<A-j>", "<Plug>MoveLineDown", { remap = true, silent = true, desc = "Move line down" })
+      vim.keymap.set(
+        "x",
+        "<A-k>",
+        "<Plug>MoveBlockCountLinesUp",
+        { remap = true, silent = true, desc = "Move selection up" }
+      )
+      vim.keymap.set(
+        "x",
+        "<A-j>",
+        "<Plug>MoveBlockCountLinesDown",
+        { remap = true, silent = true, desc = "Move selection down" }
+      )
+    end,
+  },
 
   -- Close and rename paired HTML/JSX tags.
   {
