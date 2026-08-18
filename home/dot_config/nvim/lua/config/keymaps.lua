d89b2450e17daa4ab6ff051c6a5d78ec789977c6
Savio Serra <serra.santos.s@gmail.com>
2026-08-18 18:28:05 +0000

    fix(nvim): harden Ctrl-S save keymaps

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-s>", "<cmd>write<cr>", { desc = "Save file" })
-vim.keymap.set("i", "<C-s>", function()
-  vim.cmd("write")
-end, { desc = "Save file" })
+vim.keymap.set({ "n", "i", "x", "s", "c" }, "<C-s>", "<cmd>write<cr>", { desc = "Save file" })
+vim.keymap.set({ "n", "i", "x", "s", "c" }, "<C-S>", "<cmd>write<cr>", { desc = "Save file" })
 vim.keymap.set("n", "gl", ":", { desc = "Go to line" })
