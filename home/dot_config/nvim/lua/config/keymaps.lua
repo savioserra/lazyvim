d35e722c8a1a0655a46a43661ede0cc7b0953e11
Savio Serra <serra.santos.s@gmail.com>
2026-08-18 18:13:57 +0000

    fix(nvim): restore Ctrl-S save keymaps

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<C-s>", "<cmd>write<cr>", { desc = "Save file" })
+vim.keymap.set("i", "<C-s>", function()
+  vim.cmd("write")
+end, { desc = "Save file" })
 vim.keymap.set("n", "gl", ":", { desc = "Go to line" })
